3310431a2372b2d862d07288004ea0b702501072
pcapurro42 <pcapurro@student.42nice.fr>
2024-02-20 19:06:55 +0100

    added back NICK and USER cmd

--- server/cmds/NickCMD.cpp
+++ server/cmds/NickCMD.cpp
@@ -6,7 +6,7 @@
 /*   By: pcapurro <pcapurro@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 22:20:08 by pcapurro          #+#    #+#             */
-/*   Updated: 2024/02/19 22:44:39 by pcapurro         ###   ########.fr       */
+/*   Updated: 2024/02/20 19:05:42 by pcapurro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,73 @@
 
 int Server::executeNickCommand(string cmd, int id)
 {
-    return (0);
+    int space_nb = std::count(cmd.begin(), cmd.end(), ' ');
+    if (space_nb > 1)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " typed a command with too many paramaters." << endl;
+        return (ERR_TOOMANYPARAMS);
+    }
+    else if (space_nb < 1)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " typed a command with not enough paramaters." << endl;
+        return (ERR_NEEDMOREPARAMS);
+    }
+
+    string clientname = getArgument(cmd, 1);
+    if (clientname.size() > 42)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " failed to set a nickname (invalid length)." << endl;
+        return (ERR_ERRONEUSNICKNAME);
+    }
+    if (searchClient(clientname) != -1)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " failed to set a nickname (already taken)." << endl;
+        return (ERR_NICKCOLLISION);
+    }
+
+    if (_clients_data[id - 1].authentified != true)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " failed to request (not authentified)." << endl;
+        return (ERR_NOTREGISTERED);
+    }
+    else
+    {
+        string nickname = getArgument(cmd, 1);
+        string oldnickname = _clients_data[id - 1].nickname;
+        _clients_data[id - 1].nickname = nickname;
+
+        cout << _clients_data[id - 1].nickname << " set his nickname to '" << nickname << "'." << endl;
+
+        if (_clients_data[id - 1].username.empty() != true && _clients_data[id - 1].identified != true)
+        {
+            _clients_data[id - 1].identified = true;
+
+            sendToEveryone(": " + _clients_data[id - 1].nickname + " \x1Djoined the server.\x0f\r\n", id - 1, true);
+            
+            string message = "001 " + _clients_data[id - 1].nickname + " :\r\n";
+            send(_sockets_array[id].fd, message.c_str(), message.size(), 0);
+            
+            return (0);
+        }
+
+        if (oldnickname.empty() != true)
+        {
+            for (int i = 0; i != CANALS_LIMIT; i++)
+            {
+                vector<string>::iterator j;
+
+                j = std::find(_canals[i].members.begin(), _canals[i].members.end(), oldnickname);
+                if (j != _canals[i].members.end())
+                    _canals[i].members.erase(j), _canals[i].members.push_back(nickname);
+                
+                j = std::find(_canals[i].operators.begin(), _canals[i].operators.end(), oldnickname);
+                if (j != _canals[i].operators.end())
+                    _canals[i].operators.erase(j), _canals[i].operators.push_back(nickname);
+                
+                j = std::find(_canals[i].invited.begin(), _canals[i].invited.end(), oldnickname);
+                if (j != _canals[i].invited.end())
+                    _canals[i].invited.erase(j), _canals[i].invited.push_back(nickname);
+            }
+        }
+    }
 }
--- server/cmds/UserCMD.cpp
+++ server/cmds/UserCMD.cpp
@@ -6,7 +6,7 @@
 /*   By: pcapurro <pcapurro@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 22:17:40 by pcapurro          #+#    #+#             */
-/*   Updated: 2024/02/19 22:47:06 by pcapurro         ###   ########.fr       */
+/*   Updated: 2024/02/20 19:06:36 by pcapurro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,53 @@
 
 int Server::executeUserCommand(string cmd, int id)
 {
+    int space_nb = std::count(cmd.begin(), cmd.end(), ' ');
+    if (space_nb > 4)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " typed a command with too many paramaters." << endl;
+        return (ERR_TOOMANYPARAMS);
+    }
+    else if (space_nb < 4)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " typed a command with not enough paramaters." << endl;
+        return (ERR_NEEDMOREPARAMS);
+    }
+
+    string client_username = getArgument(cmd, 1);
+    string client_realname = getArgument(cmd, 4);
+
+    if (client_realname.empty() == true || client_username.empty() == true || client_realname.size() > 42 || client_username.size() > 42)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " failed to identify (invalid length)." << endl;
+        return (ERR_UNKNOWNCOMMAND);
+    }
+
+    if (_clients_data[id - 1].authentified == false)
+    {
+        cout << "Error! " << _clients_data[id - 1].nickname << " failed to request (not authentified)." << endl;
+        return (ERR_NOTREGISTERED);
+    }
+
+    string username = getArgument(cmd, 1);
+    string realname = getArgument(cmd, 4);
+
+    if (realname[0] == ':')
+        realname = realname.c_str() + 1;
+    _clients_data[id - 1].username = username;
+    _clients_data[id - 1].realname = realname;
+
+    cout << _clients_data[id - 1].nickname << " identified as '" << username << "' (username), '" << realname << "' (realname)." << endl;
+
+    if (_clients_data[id - 1].nickname.empty() != true && _clients_data[id - 1].identified != true)
+    {
+        _clients_data[id - 1].identified = true;
+
+        sendToEveryone(": " + _clients_data[id - 1].nickname + " \x1Djoined the server.\x0f\r\n", id - 1, true);
+
+        string message = "001 " + _clients_data[id - 1].nickname + " :\r\n";
+        send(_sockets_array[id].fd, message.c_str(), message.size(), 0);
+            
+        return (0);
+    }
     return (0);
 }
