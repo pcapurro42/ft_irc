e564f3da3421375d681387cafd62ab6364146ba4
pcapurro42 <pcapurro@student.42nice.fr>
2024-02-25 23:58:21 +0100

    fixed QUIT command problem not leaving when error of parser

--- server/cmds/QuitCMD.cpp
+++ server/cmds/QuitCMD.cpp
@@ -6,7 +6,7 @@
 /*   By: pcapurro <pcapurro@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 22:20:32 by pcapurro          #+#    #+#             */
-/*   Updated: 2024/02/25 20:11:22 by pcapurro         ###   ########.fr       */
+/*   Updated: 2024/02/25 23:57:18 by pcapurro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,23 @@
 
 int Server::executeQuitCommand(std::string cmd, int id)
 {
-    int space_nb = std::count(cmd.begin(), cmd.end(), ' ');
-    if (space_nb > 1)
+    if (std::count(cmd.begin(), cmd.end(), ' ') != 0)
     {
-        std::cout << getTime() << "Error! " << _clients_data[id].nickname << " typed a command with too many paramaters." << std::endl;
-        return (ERR_TOOMANYPARAMS);
+        std::string message = getArgument(cmd, 1);
+        if (message[0] == ':')
+            message = message.c_str() + 1;
+
+        std::cout << getTime() << _clients_data[id].nickname << " left the server saying '" << message << "' (" << _clients_slots - 1 << "/" << MAX_CLIENTS << ")." << std::endl;
+
+        std::string msg = _clients_data[id].nickname + " \x1Dleft the server saying '" + message + "'\x0f.\r\n";
+        sendToEveryone(msg, id + 1, true);
+        removeClient(id + 1);
     }
     else
     {
-        if (std::count(cmd.begin(), cmd.end(), ' ') != 0)
-        {
-            std::string message = getArgument(cmd, 1);
-            if (message[0] == ':')
-                message = message.c_str() + 1;
-
-            std::cout << getTime() << _clients_data[id].nickname << " left the server saying '" << message << "' (" << _clients_slots - 1 << "/" << MAX_CLIENTS << ")." << std::endl;
-
-            std::string msg = _clients_data[id].nickname + " \x1Dleft the server saying '" + message + "'\x0f.\r\n";
-            sendToEveryone(msg, id + 1, true);
-            removeClient(id + 1);
-        }
-        else
-        {
-            std::string msg = _clients_data[id].nickname + " \x1Dleft the server\x0f.\r\n";
-            sendToEveryone(msg, id + 1, true);
-            removeClient(id + 1);
-        }
+        std::string msg = _clients_data[id].nickname + " \x1Dleft the server\x0f.\r\n";
+        sendToEveryone(msg, id + 1, true);
+        removeClient(id + 1);
     }
     return (0);
 }
