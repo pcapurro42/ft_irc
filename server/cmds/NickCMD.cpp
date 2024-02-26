d7060380e101398034408f862719cbc351918a1b
bgales <bjmgales@gmail.com>
2024-02-26 14:10:55 +0100

    Fix: off bot was sending message at server start

--- server/cmds/NickCMD.cpp
+++ server/cmds/NickCMD.cpp
@@ -71,25 +71,26 @@ int Server::executeNickCommand(std::string cmd, int id)
         
         std::string message = "Welcome to our IRC server!\r\n";
         send(_sockets_array[id + 1].fd, message.c_str(), message.size(), 0);
-    
+
         message = "Here are the availaible commands: PASS, USER, NICK, KICK, INVITE, JOIN, PING, QUIT, TOPIC AND BOT.\n";
         send(_sockets_array[id + 1].fd, message.c_str(), message.size(), 0);
+        if (BOT == 1){
+            message = "Welcome dear " + _clients_data[id].nickname + "! I'm Gipiti, the time bot !\r\n";
+            std::string msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
+            send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
         
-        message = "Welcome dear " + _clients_data[id].nickname + "! I'm Gipiti, the time bot !\r\n";
-        std::string msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
-        send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
-        
-        message = "Type '!time' to ask for time.\r\n";
-        msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
-        send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
+            message = "Type '!time' to ask for time.\r\n";
+            msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
+            send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
 
-        message = "Type '!date' to ask for data.\r\n";
-        msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
-        send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
+            message = "Type '!date' to ask for data.\r\n";
+            msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
+            send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
 
-        message = "Here are the availaible commands: PASS, USER, NICK, KICK, INVITE, JOIN, PING, QUIT, TOPIC AND BOT.\r\n";
-        msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
-        send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
+            message = "Here are the availaible commands: PASS, USER, NICK, KICK, INVITE, JOIN, PING, QUIT, TOPIC AND BOT.\r\n";
+            msg = ":Gipiti_bot PRIVMSG " + _clients_data[id].nickname + " :" + message;
+            send(_sockets_array[id + 1].fd, msg.c_str(), msg.size(), 0);
+        }
             return (0);
     }
 
