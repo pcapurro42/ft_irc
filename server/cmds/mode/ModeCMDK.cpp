55f3ee3d67251863c558ded7cdf3fb0030deeab4
bgales <bjmgales@gmail.com>
2024-02-26 18:23:14 +0100

    add:canal msg for mode changes(+k/-k)

--- server/cmds/mode/ModeCMDK.cpp
+++ server/cmds/mode/ModeCMDK.cpp
@@ -23,12 +23,15 @@ void    Server::executeModeKCommand(std::string password, int canal_id, int id, 
             _canals[canal_id].pass_only = true;
             _canals[canal_id].password = password;
             std::cout << getTime() << _clients_data[id].nickname << " changed " << _canals[canal_id].name << "'s access to password only." << std::endl;
-        }
+            std::string message = ":" + _clients_data[id].nickname + " MODE " + _canals[canal_id].name + " +k " + password + "\r\n";
+            sendToEveryChannelMembers(message, _canals[canal_id].name);}
         if (sign == '-' && _canals[canal_id].pass_only == true)
         {
             _canals[canal_id].pass_only = false;
             _canals[canal_id].password = "";
             std::cout << getTime() << _clients_data[id].nickname << " changed " << _canals[canal_id].name << "'s access to public." << std::endl;
+            std::string message = ":" + _clients_data[id].nickname + " MODE " + _canals[canal_id].name + " -k\r\n";
+            sendToEveryChannelMembers(message, _canals[canal_id].name);
         }
     }
 }
