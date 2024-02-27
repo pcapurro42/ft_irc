5bdc6342899d6b90b23cffe76720d7ce5454824f
pcapurro42 <pcapurro@student.42nice.fr>
2024-02-28 00:33:32 +0100

    voided cmd in executePongCommand() to permit compilation with flags

--- server/cmds/PongCMD.cpp
+++ server/cmds/PongCMD.cpp
@@ -6,7 +6,7 @@
 /*   By: pcapurro <pcapurro@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:23:03 by pcapurro          #+#    #+#             */
-/*   Updated: 2024/02/28 00:18:33 by pcapurro         ###   ########.fr       */
+/*   Updated: 2024/02/28 00:32:51 by pcapurro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int Server::executePongCommand(std::string cmd, int id)
 {
+    (void) cmd;
     _clients_data[id].ping = true;
     // std::cout << getTime() << "PONG received from " << _clients_data[id].nickname << "." << std::endl;
     return (0);
