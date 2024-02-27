790aff492ac0647240730f4bb9ac9a6262ecf0ff
pcapurro42 <pcapurro@student.42nice.fr>
2024-02-28 00:20:29 +0100

    simplified PING command

--- server/cmds/PingCMD.cpp
+++ server/cmds/PingCMD.cpp
@@ -6,7 +6,7 @@
 /*   By: pcapurro <pcapurro@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 21:13:52 by pcapurro          #+#    #+#             */
-/*   Updated: 2024/02/25 22:49:18 by pcapurro         ###   ########.fr       */
+/*   Updated: 2024/02/28 00:20:10 by pcapurro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,16 @@
 
 int Server::executePingCommand(std::string cmd, int id)
 {
-    if (std::count(cmd.begin(), cmd.end(), ' ') != 0)
-    {
-        std::cout << getTime() << "Error! " << _clients_data[id].nickname << " typed an invalid or unsupported command." << std::endl;
-        return (ERR_UNKNOWNCOMMAND);
-    }
+    std::cout << getTime() << "PING received from " << _clients_data[id].nickname << ". Sending PONG..." << std::endl;
+
+    std::string message;
+    if (getArgument(cmd, 1) != "")
+        message = "PONG " + getArgument(cmd, 1) + "\r\n";
     else
-    {
-        std::cout << getTime() << "PING received from " << _clients_data[id].nickname << ". Sending PONG..." << std::endl;
- 
-        std::string message = "PONG " + getArgument(cmd, 1) + "\r\n";
-        send(_sockets_array[id + 1].fd, message.c_str(), message.size(), 0);
-        
-        std::cout << getTime() << "PONG sent to " << _clients_data[id].nickname << "." << std::endl;
-    }
+        message = "PONG\r\n";
+    send(_sockets_array[id + 1].fd, message.c_str(), message.size(), 0);    
+    
+    std::cout << getTime() << "PONG sent to " << _clients_data[id].nickname << "." << std::endl;
+    
     return (0);
 }
