3eee7a143d8672fd3f88a09fdb793f269ff32e14
pcapurro42 <pcapurro@student.42nice.fr>
2024-02-28 00:18:45 +0100

    simplified PONG command

--- server/cmds/PongCMD.cpp
+++ server/cmds/PongCMD.cpp
@@ -6,7 +6,7 @@
 /*   By: pcapurro <pcapurro@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:23:03 by pcapurro          #+#    #+#             */
-/*   Updated: 2024/02/25 19:38:21 by pcapurro         ###   ########.fr       */
+/*   Updated: 2024/02/28 00:18:33 by pcapurro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,7 @@
 
 int Server::executePongCommand(std::string cmd, int id)
 {
-    if (std::count(cmd.begin(), cmd.end(), ':') != 1)
-    {
-        std::cout << getTime() << "Error! " << _clients_data[id].nickname << " typed an invalid or unsupported command." << std::endl;
-        return (ERR_UNKNOWNCOMMAND);
-    }
-    else
-    {
-        _clients_data[id].ping = true;
-        // std::cout << getTime() << "PONG received from " << _clients_data[id].nickname << "." << std::endl;
-    }
+    _clients_data[id].ping = true;
+    // std::cout << getTime() << "PONG received from " << _clients_data[id].nickname << "." << std::endl;
     return (0);
 }
